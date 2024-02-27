f53087e39e5062958754b896f7160fcfffa31da5
fkuraylo <lipbalm911@gmail.com>
2024-02-27 22:28:25 +0300

    Web routes for CRUD

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,30 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index() {
-        return 123;
+        return 'Index action';
+    }
+
+    public function create() {
+        return 'Create form action';
+    }
+
+    public function store() {
+        return 'Save to db action';
+    }
+
+    public function show() {
+        return 'show one action';
+    }
+
+    public function edit() {
+        return 'Edit form action';
+    }
+
+    public function update() {
+        return 'Save editing to db';
+    }
+
+    public function destroy() {
+        return 'delete obj fr db';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::get('/', function () {
 });
 
 Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('product.create');
+Route::post('/products', [ProductController::class, 'store'])->name('product.store');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('product.show');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+Route::patch('/products/{product}', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
